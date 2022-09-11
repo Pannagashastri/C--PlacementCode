536d5f0da4b33ab5351fc1932e11fdc1c60d63c8
Pannagashastri <ios.pannagashastri@gmail.com>
2022-09-11 21:48:07 +0530

    increament/decreament

---
+++ operator,for loops,bitwise/increament/decreament.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // //post increament i++
+    // //pre increament i--
+    int i=4;
+    cout<<(++i)<<endl;//5
+     cout<<(i++)<<endl;//5
+     cout<<(i--)<<endl;//6
+     cout<<(--i)<<endl;//4
+
+     int a=1;
+     int b=a++;
+     int c=++a;
+     cout<<b<<endl;
+     cout<<c<<endl;
+}
