d0002f2e22df8fb0d2b24ef50b7cac61c093ba3b
Pannagashastri <ios.pannagashastri@gmail.com>
2022-09-11 23:31:28 +0530

    nextContinue

---
+++ operator,for loops,bitwise/Continuee.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+        cout<<"hi"<<endl;
+        cout<<"hello"<<endl;
+        continue;
+        cout<<"fuck you"<<endl;
+    }
+    // for(int i=0;i<n;i--){
+    //     cout<<i<<" ";
+    //     i++;
+    // }
+}
--- operator,for loops,bitwise/Prime.cpp
+++ operator,for loops,bitwise/Prime.cpp
@@ -4,4 +4,21 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
+    bool isPrime=1; 
+    for(int i=2;i<n;i++){
+        if(n%i==0){
+            //cout<<"not a prime number"<<endl;
+            isPrime=0;
+            break;
+        }
+
+
+        
+    }
+    if(isPrime==0){
+        cout<<"not a prime"<<endl;
+        
+    }else{
+        cout<<"prime number"<<endl;
+    }
 }
