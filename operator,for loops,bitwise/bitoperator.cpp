f3cc4f5eafb9707108509ee3b4cfdad07fe84af3
Pannagashastri <ios.pannagashastri@gmail.com>
2022-09-11 21:28:26 +0530

    endingOfBitOpperator

--- operator,for loops,bitwise/bitoperator.cpp
+++ operator,for loops,bitwise/bitoperator.cpp
@@ -7,9 +7,14 @@ int main(){
     cout<<"a&b"<<(a&b)<<endl;
     cout<<"a|b"<<(a|b)<<endl;
     cout<<"a^b"<<(a^b)<<endl;
-    //5<<2 = 5/2*2; 5<<1=5/2;
-    //5>>2=5*2*2;
+    cout<<"~a"<<~a<<endl;
+    //5<<2 = 5*2*2; 5<<1=5*2;
+    //17>>1==8(17/2==8);17>>2==4(17/2*2==4
     //padding with zero
     //for negative padding will be compiler dependent
 
-}
+    cout<<(17>>1)<<endl;//divide
+    cout<<(17>>2)<<endl;//divide
+    cout<<(5<<2)<<endl;//multiply
+    cout<<(5<<1)<<endl;//multiply
+}   
