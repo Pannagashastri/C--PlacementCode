87dfb754d877b7449dcd373806878e39b9a1f6f2
Pannagashastri <ios.pannagashastri@gmail.com>
2022-09-15 22:44:06 +0530

    arraySwapAlternate

---
+++ Arrays/swapAlternate.cpp
@@ -0,0 +1,35 @@
+//Algo
+//for(index->0 to n-1)
+//if(i+1<size)
+//swap(arr[i],arr[i+1]);
+#include<iostream>
+using namespace std;
+
+void printArray(int arr[],int n){
+for(int i=0;i<n;i++){
+    cout<<arr[i]<<" ";
+
+}
+cout<<endl;
+}
+void swapAlternate(int arr[],int n){
+for(int i=0;i<n;i+=2){
+    if(i+1<n){
+        swap(arr[i],arr[i+1]);
+    }
+}
+}
+
+
+int main(){
+
+    int even[8]={5,2,4,5,22,66,88,69};
+    int odd[5]={1,2,3,5,6};
+
+    swapAlternate( even,8);
+    swapAlternate(odd,5);
+
+    printArray(even,8);
+    printArray(odd,5);
+return 0;
+}
