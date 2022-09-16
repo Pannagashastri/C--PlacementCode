b3a17b87cdeed4e34a255984c6ebf7c5ca3fa2ff
Pannagashastri <ios.pannagashastri@gmail.com>
2022-09-17 00:25:01 +0530

    17th

--- Arrays/arrayIntercetpoin.cpp
+++ Arrays/arrayIntercetpoin.cpp
@@ -1,7 +1,17 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+void printArray(int arr[],int n){
+    int element;
+    for(int i=0;i<n;i++){
+         element=arr[i];
+        cout<<element;
+    }
+}
 
+
+int main(){
+int mm[10]={1,3,4,4,5,5};
+printArray(mm,5);
     return 0;
 }
