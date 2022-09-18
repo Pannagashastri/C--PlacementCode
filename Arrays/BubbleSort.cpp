2fce0b23068f51a2383ac8238e7d76e00ddc3706
Pannagashastri <ios.pannagashastri@gmail.com>
2022-09-18 19:15:27 +0530

    7thBubble

---
+++ Arrays/BubbleSort.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    for(int i=0;i<n;i++){
+
+        //process to n-1
+        for(int j=0;j<n-i;j++){
+
+            //process element till n-i th index 
+                if(arr[j]>arr[j+1]){
+                    swap(arr[j],arr[j+1]);
+                }
+        }
+    }
+
+    return 0;
+}
