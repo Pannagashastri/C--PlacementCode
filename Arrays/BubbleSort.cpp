9bb261a26d60590e947a61d534ae50252be30277
Pannagashastri <ios.pannagashastri@gmail.com>
2022-09-19 20:06:30 +0530

    8thDay

--- Arrays/BubbleSort.cpp
+++ Arrays/BubbleSort.cpp
@@ -1,18 +1,38 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
-int main(){
-
-    for(int i=0;i<n;i++){
-
-        //process to n-1
+void BubbleSort(vector<int>&arr, int n){
+    for(int i=1;i<n;i++){
+        bool swapped=false;
         for(int j=0;j<n-i;j++){
-            //process element till n-i th index 
-                if(arr[j]>arr[j+1]){
-                    swap(arr[j],arr[j+1]);
-                }
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+                swapped=true;
+        
+            }
+        }
+        if(swapped==false){
+            //already sorted
+            break;
         }
     }
+}
 
-    return 0;
+int main(){
+    int even[6]={1,33,3,22,10,12};
+
+    BubbleSort(even[6],6);
+    // for(int i=0;i<n;i++){
+
+    //     //process to n-1
+    //     for(int j=0;j<n-i;j++){
+    //         //process element till n-i th index 
+    //             if(arr[j]>arr[j+1]){
+    //                 swap(arr[j],arr[j+1]);
+    //             }
+    //     }
+    // }
+
+    // return 0;
 }
