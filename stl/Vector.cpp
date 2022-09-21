536686be2db9e39d3f585f50a9d737b414c5358d
Pannagashastri <ios.pannagashastri@gmail.com>
2022-09-21 18:57:30 +0530

    sfsf

--- stl/Vector.cpp
+++ stl/Vector.cpp
@@ -4,9 +4,14 @@ using namespace std;
 
 int main(){
 
-    vector<int> v;
-    cout<<"size is "<<v.capacity()<<endl;
-    v.push_back(1);
-    cout<<"size"<<v.capacity()<<endl;
-    v.push_back()
+    // vector<int> v;
+    // cout<<"size is "<<v.capacity()<<endl;
+    // v.push_back(1);
+    // cout<<"size"<<v.capacity()<<endl;
+    // v.push_back()
+
+    vector<int> a(5,1);
+    for(int i:a){
+        cout<<i<<" ";
+    }
 }
