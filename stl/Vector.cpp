ddfb066d72933dc6e218096f92360a761a5fbce8
Pannagashastri <ios.pannagashastri@gmail.com>
2022-09-21 20:12:36 +0530

    9thDay

--- stl/Vector.cpp
+++ stl/Vector.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 #include<vector>
+#include<deque>
+#include<queue>
 using namespace std;
 
 int main(){
@@ -10,8 +12,24 @@ int main(){
     // cout<<"size"<<v.capacity()<<endl;
     // v.push_back()
 
-    vector<int> a(5,1);
-    for(int i:a){
-        cout<<i<<" ";
-    }
+    // vector<int> a(5,1);
+    // for(int i:a){
+    //     cout<<i<<" ";
+    // }
+    // deque<int> d;
+    // d.push_back(1);
+
+    // d.push_front(2);
+
+    // for(int i:d){
+    //     cout<<i<<" ";
+    // }
+
+    //max heap
+priority_queue<int> 
+
+//min heap
+priority_queue<int,vector<int>,greater<int> > mini;
+
+
 }
